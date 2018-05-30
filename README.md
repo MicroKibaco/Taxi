1f9d0bdc68918f9c08fc2676436dfbc4474b14c6
github <569015640@qq.com>
2018-05-30 16:20:01 +0800

    SVG 制造logo

--- README.md
+++ README.md
@@ -1,7 +1,25 @@
 # 滴滴打车
 
-- ## SVG 用法
+## 章节一: 启动过渡页面开发
+### A. SVG 用法
 SVG 有何优点?
 - SVG 可被非常多的g工具读取和修改
+
 - SVG 与 JPEG 和 GIF 图像比起来,尺寸更小,可压缩性更强
+
+ <img src="./doc/icon_logo.png" />
+ 
+### B. SVG 制造logo
+- M = moveTo (M X,Y) : 将画笔移动到指定的坐标位置
+
+- L = lineTo(L X,Y) : 画直线到指定的坐标位置
+
+- H = horizontal lineTo(H X) : 画水平线到指定的X坐标位置
+
+- V = vertical lineTo(V Y) : 画垂直线到指定的Y坐标位置
+
+- A = elliptical Arc(A RX, RY, XROTATION, FLAG1, FLAG2,X,Y) : 弧线
+
+
+
 
---
+++ app/src/main/java/com/github/microkibaco/taxi/SplashActivity.java
@@ -0,0 +1,30 @@
+package com.github.microkibaco.taxi;
+
+import android.annotation.SuppressLint;
+import android.graphics.drawable.AnimatedVectorDrawable;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatImageView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Bind(R.id.iv_logo)
+    AppCompatImageView mLogo;
+
+    @SuppressLint("ObsoleteSdkInt")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        ButterKnife.bind(this);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            final AnimatedVectorDrawable anim = (AnimatedVectorDrawable) getResources().getDrawable(R.drawable.anim);
+            mLogo.setImageDrawable(anim);
+            anim.start();
+        }
+    }
+}
