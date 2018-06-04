f8e2d4b81ffd99d65a4726171186bda60ac64c93
github <569015640@qq.com>
2018-06-04 14:41:45 +0800

    RxJava的使用以及RxJava的封装

--- app/src/main/java/com/github/microkibaco/taxi/account/view/SmsCodeDialog.java
+++ app/src/main/java/com/github/microkibaco/taxi/account/view/SmsCodeDialog.java
@@ -3,6 +3,7 @@ package com.github.microkibaco.taxi.account.view;
 import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.annotation.StyleRes;
@@ -16,38 +17,32 @@ import android.view.View;
 import com.dalimao.corelibrary.VerificationCodeInput;
 import com.github.microkibaco.taxi.R;
 import com.github.microkibaco.taxi.TaxiApplication;
+import com.github.microkibaco.taxi.account.model.AccountManagerImpl;
 import com.github.microkibaco.taxi.account.presenter.ISmsCodeDialogPresenter;
+import com.github.microkibaco.taxi.account.presenter.SmsCodeDialogPresenterImpl;
+import com.github.microkibaco.taxi.common.databus.RxBus;
 import com.github.microkibaco.taxi.common.http.IHttpClient;
 import com.github.microkibaco.taxi.common.http.impl.OkHttpClientImpl;
 import com.github.microkibaco.taxi.common.storage.SharedPreferencesDao;
 import com.github.microkibaco.taxi.main.MainActivity;
 
-import butterknife.Bind;
 
-
-public class SmsCodeDialog extends Dialog implements ISmsCodeDialogView {
+public class SmsCodeDialog extends Dialog implements ISmsCodeDialogView, View.OnClickListener, VerificationCodeInput.Listener {
     private static final String TAG = SmsCodeDialog.class.getSimpleName();
 
     private String mPhone;
-    @Bind(R.id.close)
-    AppCompatImageView mClose;
-    @Bind(R.id.dialog_title)
-    AppCompatTextView mDialogTitle;
-    @Bind(R.id.phone)
-    AppCompatTextView mPhoneTv;
-    @Bind(R.id.btn_resend)
-    AppCompatButton mBtnResend;
-    @Bind(R.id.verificationCodeInput)
-    VerificationCodeInput mVerificationCodeInput;
-    @Bind(R.id.loading)
-    ContentLoadingProgressBar mLoading;
-    @Bind(R.id.error)
-    AppCompatTextView mError;
+    private AppCompatImageView mClose;
+    private AppCompatTextView mDialogTitle;
+    private AppCompatTextView mPhoneTv;
+    private AppCompatButton mBtnResend;
+    private VerificationCodeInput mVerificationCodeInput;
+    private ContentLoadingProgressBar mLoading;
+    private AppCompatTextView mError;
 
     private ISmsCodeDialogPresenter mPresenter;
     private MainActivity mainActivity;
 
-    public SmsCodeDialog(MainActivity context, String phone) {
+    SmsCodeDialog(MainActivity context, String phone) {
         this(context, R.style.Dialog);
         // 上一个界面传来的手机号码
         this.mPhone = phone;
@@ -55,7 +50,9 @@ public class SmsCodeDialog extends Dialog implements ISmsCodeDialogView {
         final SharedPreferencesDao dao =
                 new SharedPreferencesDao(TaxiApplication.getInstance(),
                         SharedPreferencesDao.FILE_ACCOUNT);
-
+        final AccountManagerImpl iAccountManager = new AccountManagerImpl(httpClient, dao);
+        mPresenter = new SmsCodeDialogPresenterImpl(this, iAccountManager);
+        this.mainActivity = context;
 
     }
 
@@ -66,16 +63,52 @@ public class SmsCodeDialog extends Dialog implements ISmsCodeDialogView {
                 (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         final View root = inflater.inflate(R.layout.dialog_smscode_input, null);
         setContentView(root);
-        final String template = getContext().getString(R.string.sending);
-        mPhoneTv.setText(String.format(template, mPhone));
+        initView();
+        initListener();
 
+
+    }
+
+    @Override
+    public void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        mCountDownTimer.cancel();
+    }
+
+    private void initListener() {
+        // 注册 Presenter
+        RxBus.getInstance().register(mPresenter);
+
+        // 关闭按钮组件监听器
+        mClose.setOnClickListener(this);
+
+        // 验证码输入完成监听
+        mVerificationCodeInput.setOnCompleteListener(this);
+    }
+
+    @Override
+    public void dismiss() {
+        super.dismiss();
+        // 注销 Presenter
+        RxBus.getInstance().unRegister(mPresenter);
+    }
+
+    private void initView() {
+        mPhoneTv = (AppCompatTextView) findViewById(R.id.phone);
+        mBtnResend = (AppCompatButton) findViewById(R.id.btn_resend);
+        mVerificationCodeInput = (VerificationCodeInput) findViewById(R.id.verificationCodeInput);
+        mLoading = (ContentLoadingProgressBar) findViewById(R.id.loading);
+        mError = (AppCompatTextView) findViewById(R.id.error);
+        mError.setVisibility(View.GONE);
+        mPhoneTv.setText(String.format(getContext().getString(R.string.sending), mPhone));
+        mClose = (AppCompatImageView) findViewById(R.id.close);
     }
 
     public SmsCodeDialog(@NonNull Context context) {
         super(context);
     }
 
-    public SmsCodeDialog(@NonNull Context context, @StyleRes int themeResId) {
+    private SmsCodeDialog(@NonNull Context context, @StyleRes int themeResId) {
         super(context, themeResId);
     }
 
@@ -95,7 +128,10 @@ public class SmsCodeDialog extends Dialog implements ISmsCodeDialogView {
 
     @Override
     public void showCountDownTimer() {
-
+        mPhoneTv.setText(String.format(getContext()
+                .getString(R.string.sms_code_send_phone), mPhone));
+        mCountDownTimer.start();
+        mBtnResend.setEnabled(false);
     }
 
     @Override
@@ -107,4 +143,43 @@ public class SmsCodeDialog extends Dialog implements ISmsCodeDialogView {
     public void showUserExist(boolean b) {
 
     }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.close:
+                dismiss();
+                break;
+
+            case R.id.btn_resend:
+                resend();
+                break;
+        }
+    }
+
+    private void resend() {
+
+    }
+
+    @Override
+    public void onComplete(String s) {
+
+    }
+
+    private CountDownTimer mCountDownTimer = new CountDownTimer(60000, 1000) {
+        @Override
+        public void onTick(long millisUntilFinished) {
+            mBtnResend.setEnabled(false);
+            mBtnResend.setText(String.format(getContext()
+                    .getString(R.string.after_time_resend,
+                            millisUntilFinished / 1000)));
+        }
+
+        @Override
+        public void onFinish() {
+            mBtnResend.setEnabled(true);
+            mBtnResend.setText(getContext().getString(R.string.resend));
+            cancel();
+        }
+    };
 }
